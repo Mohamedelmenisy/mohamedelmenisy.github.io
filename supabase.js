c398a84b0f5bed9546adb69f6897f0c986019706
Mohamed Elmenisy <m.elsayed@thechefz.co>
2025-09-25 16:36:17 +0300

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,8 @@
+javascript
+// supabase.js
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+
+const supabaseUrl = 'https://aefiigottnlcmjzilqnh.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFlZmlpZ290dG5sY21qemlscW5oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcxNzY2MDQsImV4cCI6MjA2Mjc1MjYwNH0.FypB02v3tGMnxXV9ZmZMdMC0oQpREKOJWgHMPxUzwX4';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
