27d310acb0ce08545845b38a7bb59460d8be98c0
Mohamed Elmenisy <m.elsayed@thechefz.co>
2025-10-09 05:47:29 +0300

    Update busy_new.js

--- busy_new.js
+++ busy_new.js
@@ -151,7 +151,6 @@
     // ===== Initialization Function =====
     function init() {
         lazyLoadMedia();
-        // Attempt to set up calculator; it will do nothing if elements aren't found
         setupCalculator('en');
         setupCalculator('ar');
     }
